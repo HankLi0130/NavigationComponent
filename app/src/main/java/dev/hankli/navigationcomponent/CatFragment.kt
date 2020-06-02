dc1d800f1bc5e2ea04c8bfdd4cc7fd71128b0583
Hank Li <HankLi0130@gmail.com>
2020-06-02 18:59:18 -0400

    Before start

---
+++ app/src/main/java/dev/hankli/navigationcomponent/CatFragment.kt
@@ -0,0 +1,12 @@
+package dev.hankli.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class CatFragment : Fragment(R.layout.fragment_cat) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/dev/hankli/navigationcomponent/DogFragment.kt
@@ -0,0 +1,12 @@
+package dev.hankli.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DogFragment : Fragment(R.layout.fragment_dog) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
