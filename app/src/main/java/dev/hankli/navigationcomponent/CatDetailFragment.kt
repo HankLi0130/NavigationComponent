b189da2ef43b37e93d7ff221aa68a95ababea4d5
Hank Li <HankLi0130@gmail.com>
2020-06-03 15:29:37 -0400

    Add dog and cat detail fragments

---
+++ app/src/main/java/dev/hankli/navigationcomponent/CatDetailFragment.kt
@@ -0,0 +1,12 @@
+package dev.hankli.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class CatDetailFragment : Fragment(R.layout.fragment_cat_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/dev/hankli/navigationcomponent/DogDetailFragment.kt
@@ -0,0 +1,12 @@
+package dev.hankli.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DogDetailFragment : Fragment(R.layout.fragment_dog_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
