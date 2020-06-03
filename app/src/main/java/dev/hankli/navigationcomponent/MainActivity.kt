565d4500e27280f6c28147d47e73231366056de3
Hank Li <HankLi0130@gmail.com>
2020-06-02 21:03:40 -0400

    Add navigation graph Setup NavHostFragment Setup ActionBar with navigation UI

--- app/src/main/java/dev/hankli/navigationcomponent/MainActivity.kt
+++ app/src/main/java/dev/hankli/navigationcomponent/MainActivity.kt
@@ -2,11 +2,29 @@ package dev.hankli.navigationcomponent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val navController = findNavController()
+        appBarConfig = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController().navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+
+    private fun findNavController(): NavController {
+        return findNavController(R.id.nav_host_fragment)
     }
 }
--- app/src/main/java/dev/hankli/navigationcomponent/MainFragment.kt
+++ app/src/main/java/dev/hankli/navigationcomponent/MainFragment.kt
@@ -3,11 +3,20 @@ package dev.hankli.navigationcomponent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view_btn_dog.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_dogFragment)
+        }
+
+        view_btn_cat.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_catFragment)
+        }
     }
 }
